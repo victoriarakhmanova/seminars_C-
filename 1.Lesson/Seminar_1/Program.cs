285d99877ded1568cd1c1208026ac980c1655347
Victoria Rakhmanova <tori.84.07.15@mail.ru>
2022-09-09 01:15:41 +0300

    Seminar 1

---
+++ 1.Lesson/Seminar_1/Program.cs
@@ -0,0 +1,50 @@
+﻿
+/*Задача 1. 
+
+Console.Write("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number * number;
+
+Console.WriteLine($"Quad of {number} is {kvadrat}");
+*/
+/*Задача 2. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+Console.Write("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine($"{num1} is not a quad of {num2}");
+
+}
+
+*/
+
+
+
+// Задача 3. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+/*
+Console.Write("Input positive integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = number * (-1);
+
+while(current <=number)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+//Задача 4 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+/*
+Console.Write("Input three-digit integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number % 10 ;
+
+Console.WriteLine($"Last digit of {number} is {result}");
+*/
